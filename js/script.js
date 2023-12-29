1ec7c731c8e858ac11954e0ef3a01c5ea02f950c
duncanmackinnon <duncan2@mackinnons.ca>
2023-12-29 01:05:51 -0500

    Updated to a new layout

--- js/script.js
+++ js/script.js
@@ -1,48 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Update the year dynamically in the footer
-    const yearSpan = document.getElementById('year');
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
-    }
+    // Update year in the footer
+    document.getElementById('year').textContent = new Date().getFullYear();
 
-    // Theme toggle functionality
-    const themeToggleButton = document.getElementById('theme-toggle');
-    if (themeToggleButton) {
-        themeToggleButton.addEventListener('click', function () {
-            // Toggle dark mode for body, header, and footer
-            const isDarkMode = document.body.classList.toggle('dark-mode');
-            const header = document.querySelector('header');
-            const footer = document.querySelector('footer');
-            if (header && footer) {
-                header.classList.toggle('dark-mode');
-                footer.classList.toggle('dark-mode');
+    // Event listeners for nav menu links
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            // Close the hamburger menu when a link is clicked
+            document.querySelector('.nav-menu ul').classList.remove('open');
+
+            // Check if the clicked link is for toggling the theme
+            if (this.getAttribute('href') === '#toggleTheme') {
+                e.preventDefault(); // Prevent default anchor behavior
+                toggleTheme();
             }
-
-            // Save the theme state in local storage
-            localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
         });
-    }
+    });
 
-    // Apply the saved theme on page load
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-        const header = document.querySelector('header');
-        const footer = document.querySelector('footer');
-        if (header && footer) {
-            header.classList.add('dark-mode');
-            footer.classList.add('dark-mode');
+    // Welcome page behavior
+    window.addEventListener('scroll', function() {
+        const welcomeSection = document.getElementById('welcome');
+        if (welcomeSection && window.pageYOffset > welcomeSection.offsetHeight) {
+            welcomeSection.style.display = 'none'; // Hide the welcome section on scroll
         }
-    }
-
-    // Add smooth scrolling to all anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
     });
 });
 
+function toggleMenu() {
+    document.querySelector('.nav-menu ul').classList.toggle('open');
+}
+
+function toggleTheme() {
+    // Toggle dark mode for body, header, and footer
+    document.body.classList.toggle('dark-mode');
+    const header = document.querySelector('header');
+    const footer = document.querySelector('footer');
+    if (header && footer) {
+        header.classList.toggle('dark-mode');
+        footer.classList.toggle('dark-mode');
+    }
+}
